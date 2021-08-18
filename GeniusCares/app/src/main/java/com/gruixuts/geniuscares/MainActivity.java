eb6de56bd450f16aca98ac71f63cdb4e78965e6c
Pau <pau.sust@gmail.com>
2021-08-18 11:50:18 +0200

    Un boto que no fa res. Funciona

---
+++ GeniusCares/app/src/main/java/com/gruixuts/geniuscares/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gruixuts.geniuscares;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToProvaColors(View view) {
+//        Intent myIntent = new Intent(MainActivity.this, act_repas_sel.class);
+//        MainActivity.this.startActivity(myIntent);
+    }
+
+
+}
